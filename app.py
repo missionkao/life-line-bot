ab75931d63320db203a5e8f9a94cb39092f94e69
Mission <mission@gaominxuande-MacBook-Air.local>
2017-05-06 21:26:59 +0800

    Init commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+YOUR_CHANNEL_SECRET = os.environ.get("SECRET")
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+
+@app.route("/")
+def hello():
+    return "hello"
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    print(YOUR_CHANNEL_ACCESS_TOKEN)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
