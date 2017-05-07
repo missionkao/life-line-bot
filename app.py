a58eaaa3aa431a9e80dee0283f122dfa75f4f024
Mission <kaominghsuan@gmail.com>
2017-05-07 12:45:32 +0800

    user_id

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print(YOUR_CHANNEL_ACCESS_TOKEN)
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -44,7 +43,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.source.user_id)
+    print("user_id:", event.source.user_id)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
