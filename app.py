8d9b756229034631e9040299e1ec65a4108f14ec
Anna Chen <annachen@Annas-MacBook-Pro.local>
2017-06-21 12:46:17 +0800

    輸入捷運站，會得到捷運站周圍餐廳推薦

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request, abort
 import requests
 from urllib.parse import quote
+import apiai
+import json
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -10,9 +12,10 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage,
+    MessageEvent, TextMessage, TemplateSendMessage,
     ButtonsTemplate, PostbackTemplateAction, LocationMessage,
-    CarouselTemplate, CarouselColumn, URITemplateAction
+    CarouselTemplate, CarouselColumn, URITemplateAction,
+    ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -33,6 +36,25 @@ payload_base = {"scope": "resourceAquire",
                 }
 temperature_rid = "1f1aaba5-616a-4a33-867d-878142cac5c4"
 rain_rid = "00fcb626-9296-4ae1-9c87-f019871755c8"
+
+API_ACCESS_TOKEN = os.environ.get("API_ACCESS_TOKEN")
+ai = apiai.ApiAI(API_ACCESS_TOKEN)
+metro_location = {
+    "中山站": "25.0494808,121.5305508",
+    "古亭站": "25.0287819,121.5293875",
+    "松江南京站": "25.0404992,121.5248012",
+    "南京復興站": "25.0500368,121.5414228",
+    "公館站": "25.0059467,121.5328129",
+    "西門站": "25.0406857,121.5097222",
+    "東門站": "25.0308339,121.5290304",
+    "忠孝新生站": "25.0383673,121.5347948",
+    "忠孝復興站": "25.0383673,121.5347948",
+    "忠孝敦化站": "25.0383673,121.5347948",
+    "市政府站": "25.0377011,121.5460339",
+    "亞東醫院站": "24.9937778,121.4544763",
+}
+
+blank_image = "https://imgur.com/u4N3wpJ.jpg"
 
 
 @app.route("/")
@@ -91,9 +113,23 @@ def handle_message(event):
             event.reply_token,
             template_message)
     else:
+        location = get_response_from_api_ai(event.message.text)
+        print("get location from api: ", location)
+        if location is "empty":
+            image_message = ImageSendMessage(
+                original_content_url=blank_image,
+                preview_image_url=blank_image
+            )
+            line_bot_api.reply_message(
+                event.reply_token, image_message)
+        columns = get_restaurants_carousel(location)
+        carousel_template = CarouselTemplate(columns=columns)
+        template_message = TemplateSendMessage(
+            alt_text="餐廳小幫手",
+            template=carousel_template
+        )
         line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text))
+            event.reply_token, template_message)
 
 
 @handler.add(MessageEvent, message=LocationMessage)
@@ -151,14 +187,30 @@ def get_restaurants_carousel(location):
         address = r["vicinity"]
         title_uri = "https://www.google.com.tw/search?q=" + quote(title)
         address_uri = "https://www.google.com.tw/maps/place/" + quote(title)
+        print(title, rating, address, title_uri, address_uri)
         carousel = CarouselColumn(title=title, text=rating, actions=[
+            # the max label text size is 20, take 15 for safety
             URITemplateAction(
-                label=title, uri=title_uri),
+                label=title[0:15], uri=title_uri),
             URITemplateAction(
-                label=address, uri=address_uri),
+                label=address[0:15], uri=address_uri),
         ])
         results.append(carousel)
     return results
+
+
+def get_response_from_api_ai(text):
+    request = ai.text_request()
+    request.lang = "zh-tw"  # optional, default value equal 'en'
+    request.session_id = "demo-session"
+    request.query = text
+    response = json.loads(request.getresponse().read())
+
+    result = response["result"]
+    parameters = result.get("parameters", "empty")
+    taipei_metro = parameters.get("taipei_metro", "empty")
+    location = metro_location.get(taipei_metro, "empty")
+    return location
 
 
 if __name__ == "__main__":
